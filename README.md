4649acfb488622b40ac1ebf779d6880c9d24b70b
Yurii Polishchuk <yurii@prezly.com>
2017-07-25 17:16:30 +0300

    get cert and key filenames from command line

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-[![Go Report Card](https://goreportcard.com/badge/github.com/chris-short/ssl-tester)](https://goreportcard.com/report/github.com/chris-short/ssl-tester)
-[![GoDoc](https://godoc.org/github.com/chris-short/ssl-tester?status.svg)](https://godoc.org/github.com/chris-short/ssl-tester)
-[![Build Status](https://travis-ci.org/chris-short/ssl-tester.svg?branch=master)](https://travis-ci.org/chris-short/ssl-tester)
-[![SSL Rating](https://sslbadge.org/?domain=ssl-tester.chrisshort.net)](https://www.ssllabs.com/ssltest/analyze.html?d=ssl-tester.chrisshort.net)
-
 # ssl-tester
+Forked from https://github.com/chris-short/ssl-tester
 
 ## Description
 A small Go app intended to help troubleshoot certificate chains.
@@ -19,10 +15,15 @@ A detailed use case that prompted the creation of this code was featured on [ope
 Installation to your $GOPATH is recommended:
 
 ```
-go get github.com/chris-short/ssl-tester
+go get github.com/yuriipolishchuk/ssl-tester
 ```
 
-A public and private key at `/etc/ssl-tester/tls.crt` and `/etc/ssl-tester/tls.key` respectively are expected. These paths can be symlinks to keypairs in another path.
+## Run
+```
+ssl-tester path_to_private_key_file path_to_public_key_file
+```
+
+If params are omitted defaults values are used: `/etc/ssl-tester/tls.crt` and `/etc/ssl-tester/tls.key`. These paths can be symlinks to keypairs in another path.
 
 If you want to compile ssl-tester for another platform you can clone this repo and use `go build`. I encourage you to read Dave Chaney's [Cross compilation with Go](https://dave.cheney.net/2015/08/22/cross-compilation-with-go-1-5) to better understand that process.
 
--- server.go
+++ server.go
@@ -1,40 +1,50 @@
 package main
 
 import (
-	"crypto/tls"
-	"log"
-	"net/http"
+    "crypto/tls"
+    "log"
+    "net/http"
+    "os"
 )
 
+func getparam(n int, fallback string) string {
+    if len(os.Args) > 1 {
+        if len(os.Args[n]) != 0 {
+            return os.Args[n]
+        }
+    }
+    return fallback
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("Strict-Transport-Security", "max-age=63072000;")
-		w.Write([]byte("<h1>Hello World!</h1>\n<h1>👋</h1>"))
-	})
-	cfg := &tls.Config{
-		MinVersion:               tls.VersionTLS12,
-		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-		PreferServerCipherSuites: true,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			// POLY1305 ciphers are not in Go 1.6 and 1.7
-			//			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			//			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-		},
-	}
-	srv := &http.Server{
-		Addr:         ":443",
-		Handler:      mux,
-		TLSConfig:    cfg,
-		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
-	}
-	log.Fatal(srv.ListenAndServeTLS("/etc/ssl-tester/tls.crt", "/etc/ssl-tester/tls.key"))
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+        w.Header().Add("Strict-Transport-Security", "max-age=63072000;")
+        w.Write([]byte("<h1>Hello World!</h1>\n<h1>👋</h1>"))
+    })
+    cfg := &tls.Config{
+        MinVersion:               tls.VersionTLS12,
+        CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+        PreferServerCipherSuites: true,
+        CipherSuites: []uint16{
+            tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+            tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+            tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+            tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+            // POLY1305 ciphers are not in Go 1.6 and 1.7
+            //          tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+            //          tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+            tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+            tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+            tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+            tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+        },
+    }
+    srv := &http.Server{
+        Addr:         ":443",
+        Handler:      mux,
+        TLSConfig:    cfg,
+        TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+    }
+    log.Fatal(srv.ListenAndServeTLS(getparam(1, "/etc/ssl-tester/tls.crt"), getparam(2, "/etc/ssl-tester/tls.key")))
 }
